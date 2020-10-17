49fdfde50d9d26d16bc5de66a9600055b6c8b460
mga-xuesheng <menguoan@126.com>
2020-10-17 11:50:32 +0800

    提交第15次

--- prc/Pcr_chongxie.py
+++ prc/Pcr_chongxie.py
@@ -73,6 +73,7 @@ class PcR:
 
     '''=============================================================================================================='''
     '''主线关卡选择'''
+
     def zhangjie_sum(self):  # 查询主线章节数
         return self.pcr_find_word_sum1(PcrData.gklist, 0)
 
@@ -95,7 +96,7 @@ class PcR:
             for x in range(temp2):
                 self.pcr_dianji(PcrData.gk_you, 1.5, 2, "")
 
-    def zhangjie_nandu(self,data):  # 主线难度选择
+    def zhangjie_nandu(self, data):  # 主线难度选择
         if data == 1:
             temp1 = self.pcr_find_word_click1(PcrData.gk_normal, PcrData.gk_normal_dj, "")
             print("选择简单模式")
@@ -111,7 +112,7 @@ class PcR:
 
     '''--------------------------------------------------------------------------------------------------------------'''
 
-    def zhuxiangkxz(self,data:str):  # 主线章节关卡选择
+    def zhuxiangkxz(self, data: str):  # 主线章节关卡选择
         if self.pcr_find_word(PcrData.zhuxian, "") == 1:
             print("")
             print("主线关卡开始选择")
@@ -120,6 +121,69 @@ class PcR:
             sleep(3)
             self.zhangjie_sum_xz(int(temp1[1]))
             sleep(1)
-            self.guanqia_xuanze(int(temp1[0]),int(temp1[1]),int(temp1[2]))
+            self.guanqia_xuanze(int(temp1[0]), int(temp1[1]), int(temp1[2]))
             print("")
             print("主线关卡结束选择")
+
+    '''=============================================================================================================='''
+    '''扫荡功能'''
+
+    def saodang_sum_qr(self, data, xc_sum):  # 扫荡次数确认
+        temp1 = self.pcr_find_pic(self.pic_config, PcrData.sdjmqr, xc_sum)
+        if temp1 == 1:
+            temp2 = self.pcr_find_word_sum1(PcrData.sd_sum, 0)
+            if temp2 < data:
+                for x in range(data - temp2):
+                    self.pcr_dianji(PcrData.sdsumzj, 0.5, 1, xc_sum)
+            elif temp2 > data:
+                for x in range(temp2 - data):
+                    self.pcr_dianji(PcrData.sdsumjs, 0.5, 1, xc_sum)
+            return 1
+        return 0
+
+    def saodangtc(self):  # 扫荡弹窗确认
+        temp1 = [[self.pcr_find_pic, (tuple(self.pic_config), tuple(PcrData.sdtg), 1)],
+                 [self.pcr_find_pic, (tuple(self.pic_config), tuple(PcrData.sdok), 2)],
+                 [self.pcr_find_pic, (tuple(self.pic_config), tuple(PcrData.tssdjs), 3)]]
+        temp1_temp = self.pcr.duoxianc(temp1)
+        temp2 = [[self.pcr_find_pic_click, (tuple(self.pic_config)), tuple(PcrData.xdsd), tuple(PcrData.xdsdqx), 1],
+                 [self.pcr_find_pic_click, (tuple(self.pic_config)), tuple(PcrData.tdztc), tuple(PcrData.tdzqx), 2],
+                 [self.pcr_find_pic_click, (tuple(self.pic_config)), tuple(PcrData.djtstc), tuple(PcrData.djtsqr), 3]]
+        temp2_temp = self.pcr.duoxianc(temp2)
+        if temp1_temp[2] == 1:
+            return 1
+        else:
+            return 0
+
+    def saodangjs(self, data):  # 扫荡结束
+        if data == 0:
+            temp1 = 0
+            while temp1 == 0:
+                temp1 = self.pcr_find_pic(self.pic_config, PcrData.sdjmqr, "")
+                sleep(0.1)
+                if temp1 == 1:
+                    self.pcr_find_pic(self.pic_config, PcrData.sdqx, "")
+
+    def xiandingsdcs(self):  # 限定扫荡次数查询
+        temp1 = [[self.pcr_find_word, (tuple(PcrData.knsd0), 1)],
+                 [self.pcr_find_word, (tuple(PcrData.knsd1), 2)],
+                 [self.pcr_find_word, (tuple(PcrData.knsd2), 3)],
+                 [self.pcr_find_word, (tuple(PcrData.knsd3), 4)],
+                 [self.pcr_find_word, (tuple(PcrData.knsd4), 5)],
+                 [self.pcr_find_word, (tuple(PcrData.knsd5), 6)]]
+        temp1_temp = self.pcr.duoxianc(temp1)
+        for x in range(len(temp1_temp)):
+            if temp1_temp[x] == 1:
+                return x
+        return -1
+
+    def saodangcz(self):  # 扫荡重置
+        self.pcr_find_pic(self.pic_config, PcrData.sdcz1, "")
+        if self.pcr_find_pic_click(self.pic_config, PcrData.sdcz2, PcrData.sdcz3, "") == 1:
+            self.pcr_dianji(PcrData.sdcz4, 2, 4, "")
+            self.pcr_dianji(PcrData.sdcz5, 2, 4, "")
+            return self.xiandingsdcs()
+        else:
+            return 0
+
+    '''--------------------------------------------------------------------------------------------------------------'''
